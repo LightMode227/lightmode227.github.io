fadb7d55713ce557865ffcd5390d38958659cc24
LightMode227 <amb.trello@gmail.com>
2025-03-01 09:15:35 +0000

    new method of getting calendar?

--- Scripts.js
+++ Scripts.js
@@ -130,7 +130,8 @@ document.addEventListener('DOMContentLoaded', function() { /* wait until page lo
       });
   
   function closeCalendars(){ /* closes all calendars  in a document */
-        calendar.classlist.add("calendarHide");
+        calendar = document.getElementsByClassName("calendar");
+        calendar[0].classlist.add("calendarHide");
       }
   document.addEventListener("click", closeCalendars);
 });
